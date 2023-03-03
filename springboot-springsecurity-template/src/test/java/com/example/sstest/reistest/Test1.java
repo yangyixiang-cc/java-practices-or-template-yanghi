a18917e81deca6e7b11746cf83d9b9493d2eb867
yanghi <191894349@qq.com>
2023-03-03 22:33:32 +0800

    update README

--- springboot-springsecurity-template/src/test/java/com/example/sstest/reistest/Test1.java
+++ springboot-springsecurity-template/src/test/java/com/example/sstest/reistest/Test1.java
@@ -1,9 +1,11 @@
 package com.example.sstest.reistest;
 
+import com.example.sstest.bean.Menu;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.util.Set;
 
@@ -15,10 +17,19 @@ public class Test1 {
 
     @Test
     public void test1(){
-        Set keys = redisTemplate.keys("*");
-        keys.stream().forEach(o -> {
-            System.out.println(o);
-        });
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        Menu menu = new Menu();
+        menu.setMenuName("系统管理");
+        valueOperations.set("menu", menu);
+
+    }
+
+    @Test
+    public void test2(){
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        Menu menu = (Menu) valueOperations.get("menu");
+        System.out.println(menu.getMenuName());
     }
 
 }
+
